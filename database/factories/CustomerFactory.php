c3aaeb18ad3a607c9eb010cd7867d48e40f5a67b
Thomas Buckle <thomasbuckle.uk@outlook.com>
2024-03-28 09:32:05 +0000

    Create customer migration, model and factory

---
+++ database/factories/CustomerFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Customer;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CustomerFactory extends Factory
+{
+    protected $model = Customer::class;
+
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name,
+            'registration' => ['RG85 RFU']
+        ];
+    }
+}
