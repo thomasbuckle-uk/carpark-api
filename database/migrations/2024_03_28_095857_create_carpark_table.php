5a5bb5e2bff454da5748d34cbdc3823620160a4b
Thomas Buckle <thomasbuckle.uk@outlook.com>
2024-03-28 10:35:20 +0000

    added migration to create carpark and calendar tables

---
+++ database/migrations/2024_03_28_095857_create_carpark_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('carpark', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('total_spaces');
+            $table->timestamps();
+        });
+
+        Schema::create('pricing_calendar', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(App\Models\CarPark::class);
+            $table->string('season');
+            $table->string('day_type');
+            $table->double('price_per_day');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pricing_calendar');
+        Schema::dropIfExists('carpark');
+    }
+};
